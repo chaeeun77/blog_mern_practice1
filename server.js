65dfe10e6cc1a9fbc9ada3f025eebdd2b13edd3c
chaeeun <chaeeun77@naver.com>
2020-09-26 17:33:03 +0900

    [Add] npm start, server.js - routes와 연결

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const app = express();
 
 
 
+
+
+
+//라우터
+const profileRoutes = require('./routes/profile')
+const userRoutes = require('./routes/user')
+
+app.use('/products', profileRoutes)
+app.use('/user', userRoutes)
+
 const PORT = process.env.PORT || 7000;
 
 
